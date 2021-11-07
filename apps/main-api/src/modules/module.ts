ddeec1d0fd4fdf8c9dca1c2fbab1c395ff7a04cb
Mike Lima <mike.rodrigues.lima@gmail.com>
2021-11-06 21:24:37 -0300

    refactor(main): add modules and tests

---
+++ apps/main-api/src/modules/module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SharedModule } from '@shared/modules/module';
+
+import { HealthModule } from './health/module';
+
+@Module({
+  imports: [HealthModule, SharedModule],
+})
+export class MainModule {}
