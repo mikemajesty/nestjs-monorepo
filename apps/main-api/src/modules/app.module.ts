81d1f61b98978e5b82df511ec2b23a508b9ef5b2
Mike Lima <mike.rodrigues.lima@gmail.com>
2021-11-06 21:23:10 -0300

    refactor(main): change app.module name to module

--- apps/main-api/src/modules/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SharedModule } from '@shared/modules/module';
-
-import { HealthModule } from './health/module';
-
-@Module({
-  imports: [HealthModule, SharedModule],
-})
-export class MainModule {}
