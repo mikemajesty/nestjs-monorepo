9d87a5d0198764a3b52c0b94eb90940d948a5dff
Mike Lima <mike.lima.ext@dasa.com.br>
2021-11-07 23:47:43 -0300

    feat(shared): remove shared module

--- apps/shared/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './filters/http-exception.filter';
-export * from './interceptors/http-exception.interceptor';
-export * from './modules/module';
-export * from './utils/index';
--- apps/shared/src/modules/secrets/adapter.ts
+++
@@ -1,5 +0,0 @@
-export abstract class ISecretsService {
-  ENV: string;
-
-  port: { MAIN_API: number };
-}
