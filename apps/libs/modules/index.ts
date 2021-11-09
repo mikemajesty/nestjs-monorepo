16856d4d76d95b8108fdae96e3b96a680413c90e
Mike Lima <mike.lima.ext@dasa.com.br>
2021-11-08 22:01:36 -0300

    refactor(libs): remove unnecessary module import

--- apps/libs/modules/index.ts
+++ apps/libs/modules/index.ts
@@ -1,5 +1,4 @@
 export * from './filters/http-exception.filter';
 export * from './interceptors/http-exception.interceptor';
 export * from './modules';
-export * from './modules/module';
 export * from './utils';
