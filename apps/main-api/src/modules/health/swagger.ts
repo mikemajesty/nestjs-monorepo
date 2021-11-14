99048a7e264e29ff334f7c2da79967fd16a435ef
Mike Lima <mike.rodrigues.lima@gmail.com>
2021-11-13 21:48:25 -0300

    feat(swagger): add swaggger utils

--- apps/main-api/src/modules/health/swagger.ts
+++ apps/main-api/src/modules/health/swagger.ts
@@ -1,36 +1,14 @@
-import { ErrorModel } from '@libs/shared';
+import { Swagger } from '@libs/utils';
 
 import { name } from '../../../package.json';
 
 export class SwagggerResponse {
   static getHealth = {
-    200: {
-      content: {
-        'text/plain': {
-          schema: {
-            example: `${name} UP!!`,
-          },
-        },
-      },
-      status: 200,
-    },
-    500: {
-      schema: {
-        example: {
-          error: {
-            code: 500,
-            traceId: '<traceId>',
-            message: 'Internal Server Error',
-            timestamp: '<timestamp>',
-            path: '/health',
-          },
-        } as ErrorModel,
-      },
-      status: 500,
-    },
+    200: Swagger.defaultResponseText({ status: 200, text: `${name} UP!!` }),
+    500: Swagger.defaultResponseError({ status: 500, route: '/health', message: 'Internal Server Error' }),
   };
 }
 
 export class SwagggerRequest {
-  /** If the controller has a body.  */
+  /** If requesters has a body.  */
 }
