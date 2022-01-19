5a938042689f158d91cfb10defd7ffe01712aa4c
Mike Lima <mike.lima.ext@dasa.com.br>
2022-01-18 23:02:52 -0300

    feat(monorepo): improve configuration

--- libs/modules/tests/initialization.js
+++ libs/modules/tests/initialization.js
@@ -7,11 +7,6 @@ process.env.PORT_OTHER_API = 4000
 
 // -----------------------------------------------------------MOCK----------------------------------------------------------------//
 
-jest.mock('moment-timezone', () => jest.fn(() => ({
-  tz: () => ({
-    format: jest.fn(),
-  })
-})));
 
 jest.mock('redis', () => ({
   createClient: () => ({
