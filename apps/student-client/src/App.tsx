cda28dc531599ba6da493a0e2287db0aea099119
Mike Lima <mike.lima.ext@dasa.com.br>
2022-02-20 11:34:55 -0300

    feat(student-api): add student react

---
+++ apps/student-client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
